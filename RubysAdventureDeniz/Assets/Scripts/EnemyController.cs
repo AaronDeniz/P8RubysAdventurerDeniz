f40bc14f70519f7da59ee0519baeea532ca8e4d2
Aaron  Deniz <145504641+AaronDeniz@users.noreply.github.com>
2023-11-27 15:28:04 -0800

    almost done with animations
    im almost done with animations

--- RubysAdventureDeniz/Assets/Scripts/EnemyController.cs
+++ RubysAdventureDeniz/Assets/Scripts/EnemyController.cs
@@ -4,25 +4,62 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
 
-    Rigidbody2D rigidbody2;
+    Rigidbody2D rigidbody2d;
+
+    float timer;
+    int direction = 1;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2 = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
      void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + Time.deltaTime * speed;
+        if (vertical)
+        {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
+            position.y = Time.deltaTime * speed;
+        }
+        else
+        {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
+            position.x = position.x + Time.deltaTime * speed;
+        }
+        
+
+        rigidbody2d.MovePosition(position);
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if(player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
